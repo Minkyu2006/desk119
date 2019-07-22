5d791e64a3c5b40fcb608f6041166913b21b8198
cis-WIn <cis@broadwave.co.kr>
2019-07-22 09:19:11 +0900

    출동일지 등록화면

---
+++ src/main/java/kr/co/broadwave/desk/record/RecordController.java
@@ -0,0 +1,18 @@
+package kr.co.broadwave.desk.record;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author InSeok
+ * Date : 2019-07-22
+ * Remark : 출동일지 컨트롤러
+ */
+@Controller
+@RequestMapping("/record")
+public class RecordController {
+    @RequestMapping("/reg")
+    public String regist(){
+        return "record/recordreg";
+    }
+}
